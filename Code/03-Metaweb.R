8b8ed0df6684b7036db97a517388ab91a48bf6cb
David Beauchesne <david.beauchesne@uqar.ca>
2020-01-03 16:57:53 -0500

    add code to evaluate and export metaweb

---
+++ Code/03-Metaweb.R
@@ -0,0 +1,51 @@
+# This script predicts the metaweb of the Estuary and Gulf of St. Lawrence
+
+# Libraries
+library(iEat)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# 1. IMPORT SPECIES LIST AND TAXONOMY
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+load('./Data/Taxonomy/TaxonomyEGSL.RData')
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# 2. IMPORT FULL INTERACTION CATALOGUE
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+load('./Data/InteractionCatalogue/S0.RData')
+S0 <- S0[, c('taxon','resource','consumer')]
+colnames(S0) <- c('taxon','target','source')
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# 3. IMPORT SIMILARITY MATRICES
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+load('./Data/Similarity/Similarity_consumers.RData')
+load('./Data/Similarity/Similarity_resources.RData')
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# 4. PREDICT BINARY INTERACTIONS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+metaweb <- iEat(S0 = S0,
+                S1 = taxonomy$taxon,
+                S2 = taxonomy$taxon,
+                sourceSim = similarity.consumers,
+                targetSim = similarity.resources,
+                K = 5,
+                minSim = 0.3,
+                minWt = 1,
+                predict = 'full algorithm')
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# 5. CREATE FOOD WEB MATRIX
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+metaweb <- iEat_to_foodWeb(metaweb)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# 6. EXPORT METAWEB
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+saveRDS(metaWeb, file = './RData/metaWeb.rds')
